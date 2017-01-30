28b8fe305f635473d00eb6d2df851b7b96b7a15e
Swan <swan@reborn.com>
2017-01-30 14:56:17 -0500

    Defining URL routes & Sending Responses

--- Express/app.js
+++ Express/app.js
@@ -1,9 +1,31 @@
 // Require and instantiate express application
 var express = require('express');
 var app = express();
+var path = require('path');
 
 // Setting global app variables. In this case it's the port
 app.set('port', 3000);
+
+// GET request for root
+app.get('/', function(req, res){
+    res
+    .status(200)
+    .sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// GET request for /JSON
+app.get('/json', function(req, res){
+    res
+    .status(200)
+    .json({"jsonData" : true});
+});
+
+// GET request for /file
+app.get('/file', function(req, res){
+    res
+    .status(200)
+    .sendFile(path.join(__dirname, 'app.js'));
+});
 
 // Listening in on the specific port that was set.
 var server = app.listen(app.get('port'), function(){
