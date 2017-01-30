c5c4b87b4ecfb7010f597ad4e87a3b584bb5b9c7
Swan <swan@reborn.com>
2017-01-30 17:23:33 -0500

    Working w/ Query Strings & Posted Form Data

--- Express/api/controllers/hotels.controllers.js
+++ Express/api/controllers/hotels.controllers.js
@@ -1,11 +1,29 @@
 var hotelData = require('../data/hotel-data.json');
 
+
 module.exports.hotelsGetAll = function(req, res) {
     console.log("GET Hotel Data")
+    console.log(req.query);
+
+    var offset = 0;
+    var count = 5;
+
+    if (req.query && req.query.offset) {
+        // Parse offset query to an int and set offset equal to it
+        offset = parseInt(req.query.offset, 10);
+    }
+
+    if (req.query && req.query.count) {
+        count = parseInt(req.query.count, 10);
+    }
+
+    var returnData = hotelData.slice(offset, offset+count);
+
     res
         .status(200)
-        .json( hotelData );
+        .json( returnData );
 }
+
 
 module.exports.hotelsGetOne = function(req, res) {
     var hotelId = req.params.hotelId;
@@ -14,4 +32,13 @@ module.exports.hotelsGetOne = function(req, res) {
     res
         .status(200)
         .json( thisHotel );
+}
+
+
+module.exports.hotelsAddOne = function(req, res) {
+    console.log("POST new Hostel");
+    console.log(req.body);
+    res
+        .status(200)
+        .json(req.body);    
 }
